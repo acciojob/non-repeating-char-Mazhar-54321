d1c9e4f4d137eaa5200e8ef19d93f7367da26b8a
Mazhar-54321 <160312563+Mazhar-54321@users.noreply.github.com>
2025-05-08 07:37:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+ let map={};
+ for(let i=0;i<s.length;i++){
+	map[s[i]]=(map[s[i]]??0)+1
+ }
+ let ans = null;
+ let keys = Object.keys(map);
+ for(let i=0;i<keys.length;i++){
+	if(map[keys[i]]===1){
+		ans=keys[i];
+		break;
+	}
+ }
+ return ans;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
